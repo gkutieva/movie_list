84e40e0c3aa5d6b514c6325bf45a86aedc28c099
Jonathan Jacka <jonathanjacka@gmail.com>
2022-08-03 16:41:32 -0500

    display title on search

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,38 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+const MOVIEURL = process.env.REACT_APP_TMDB_KEY;
 
 export const Add = () => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
 
-  const onChange = (event) => {
-    event.preventDefault();
-    setSearch(event.target.value);
+  const handleChange = () => {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${MOVIEURL}&query=${search}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setResults(data.results);
+      })
+      .catch((error) => console.log(error.message));
+  };
 
-    const MOVIEURL = process.env.REACT_APP_TMDB_KEY;
-
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIEURL}&query=${search}`)
-    .then(res => res.json())
-    .then(data => {
-    
-      console.log(data);
-
-    })
-    .catch(error => 
-      console.log(error.message))
-  }
-
-
-
+  useEffect(() => {
+    if (search) {
+      handleChange();
+    }
+  }, [search]);
 
   return (
     <div className='add-page'>
       <div className='container'>
         <div className='add-content'>
           <div className='input-wrapper'>
-              <input type="text" placeholder='Search movie' value={search} onChange={onChange}/>
+            <input
+              type='text'
+              placeholder='Search movie'
+              value={search}
+              onChange={(event) => {
+                setSearch(event.target.value);
+              }}
+            />
           </div>
+        </div>
+
+        <div className='results'>
+          {results.length > 0
+            ? results.map((result) => <p>{result.original_title}</p>)
+            : 'No results found'}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
