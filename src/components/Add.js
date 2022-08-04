ca5ea6c6583652f46bbaad47ad20ee66d603b716
gkutieva <gvk4@nau.edu>
2022-08-04 09:10:31 -0700

    created some components

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,22 +3,25 @@ import React, { useState } from 'react';
 
 export const Add = () => {
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
 
-  const onChange = (event) => {
-    event.preventDefault();
-    setSearch(event.target.value);
+  const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(search);
 
     const MOVIEURL = process.env.REACT_APP_TMDB_KEY;
 
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIEURL}&query=${search}`)
     .then(res => res.json())
     .then(data => {
-    
-      console.log(data);
-
+      setResults(data.results);
+      console.log(data.results);
+      console.log(results);
     })
-    .catch(error => 
-      console.log(error.message))
+    .catch(error => {
+      console.log(error.message);
+      // setResults([]);
+    })
   }
 
 
@@ -29,10 +32,28 @@ export const Add = () => {
       <div className='container'>
         <div className='add-content'>
           <div className='input-wrapper'>
-              <input type="text" placeholder='Search movie' value={search} onChange={onChange}/>
+            <form onSubmit={handleSubmit}>
+              <input type="text" placeholder='Search movie' value={search} onChange={(event) => {
+                setSearch(event.target.value);
+              }}/>
+              <button type='submit'>sumbit</button>
+            </form>
           </div>
+          
+          {results.length > 0 ? "Length is greater than 0" : "length is 0"}
+          {/* {results.length > 0 && (
+            <ul className='results'>
+              {results.map((movie) => (
+                <li>
+                  {movie.title}
+                </li>
+              ))}
+            </ul>
+          )} */}
         </div>
       </div>
+
     </div>
+
   )
 }
