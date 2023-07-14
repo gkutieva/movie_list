8dd0c8a7f3db16b9f335a1345415e83e1ac6482f
Jonathan Jacka <jonathanjacka@gmail.com>
2023-07-14 15:54:24 -0500

    Updates dependencies and lint errors

--- src/components/Add.js
+++ src/components/Add.js
@@ -4,23 +4,21 @@ import { ResultCard } from './ResultMovieCard';
 const MOVIEURL = process.env.REACT_APP_TMDB_KEY;
 
 export const Add = () => {
-
   const [search, setSearch] = useState('');
   const [results, setResults] = useState([]);
 
-  const handleChange = () => {
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${MOVIEURL}&query=${search}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.results);
-        setResults(data.results);
-      })
-      .catch((error) => console.log(error.message));
-  };
-
   useEffect(() => {
+    const handleChange = () => {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${MOVIEURL}&query=${search}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.results);
+          setResults(data.results);
+        })
+        .catch((error) => console.log(error.message));
+    };
     if (search) {
       handleChange();
     }
@@ -44,11 +42,12 @@ export const Add = () => {
 
         <div className='results'>
           {results.length > 0
-            ? results.map((result, idx) => <ResultCard key={idx} movie={result}/>)
+            ? results.map((result, idx) => (
+                <ResultCard key={idx} movie={result} />
+              ))
             : 'No results found!'}
         </div>
       </div>
-
     </div>
   );
 };
